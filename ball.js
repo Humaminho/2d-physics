4b7a8c304fba22731bf1f6c099eea91dce995ec4
Humam <hu666mam@gmail.com>
2023-02-14 21:59:44 +0100

    in3l rbha gravity

--- ball.js
+++ ball.js
@@ -15,28 +15,10 @@ const maxHeight = window.innerHeight;
 
 const circle = Bodies.circle(maxWidth/2, 50, 50);
 
-
-const target = Matter.Bodies.circle(window.innerWidth/2, window.innerHeight/2, 1, {
-  isStatic: true,
-  render: {
-    visible: false,
-  },
-});
-
-const constraint = Matter.Constraint.create({
-  bodyA: circle,
-  pointB: { x: window.innerWidth/2, y: window.innerHeight/2 },
-  stiffness: 0.05,
-  damping: 0.05,
-});
-
 function addBodies() {
   Composite.add(engine.world, circle);
 }
 addBodies();
-
-Matter.World.add(engine.world, target, constraint);
-
 
 render.canvas.width = maxWidth;
 render.canvas.height = maxHeight;
@@ -44,3 +26,12 @@ render.canvas.height = maxHeight;
 Render.run(render);
 const runner = Runner.create();
 Runner.run(runner, engine);
+
+Bodies.applyForce(circle, {
+  x: 500,
+  y: 50
+},
+{
+  x: 200,
+  y: 200  
+})
